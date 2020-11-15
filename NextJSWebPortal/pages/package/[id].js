0629707c86d20bba14c9c2d8364b255aa1b2f96c
Jozef Siu <j.y.l.siu@students.uu.nl>
2020-11-15 13:12:11 +0100

    barebones subpage

--- NextJSWebPortal/pages/package/[id].js
+++ NextJSWebPortal/pages/package/[id].js
@@ -5,16 +5,40 @@ import {
   getPackagePaths,
   getAllPackageIds,
 } from "../../lib/packageData";
+import Footer from "../../components/footer";
+import styles from "../../styles/Home.module.css";
+import { Container, Row, Col, Jumbotron } from "react-bootstrap";
 
 export default function Package({ postData }) {
   return (
-    <div>
-      <div>{/* {postData.title} */}</div>
-      <div>{/* {postData.trustScore} */}</div>
-      <div>{JSON.stringify(postData, undefined, 2)}</div>
-      <Link href="/">
-        <a>← Back to home</a>
-      </Link>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <Head>
+          <title>TrustSECO</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Container>
+          <Row className={styles.content}>
+            <Link href="/">
+              <a>
+                <h2>TrustSECO</h2>
+              </a>
+            </Link>
+            <Col
+              offset={2}
+              md={{ span: 10, offset: 1 }}
+              className={styles.contentInside}
+            >
+              <main className={styles.main}>
+                <div>{/* {postData.title} */}</div>
+                <div>{/* {postData.trustScore} */}</div>
+                <div>{JSON.stringify(postData, undefined, 2)}</div>
+              </main>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <Footer />
     </div>
   );
 }
