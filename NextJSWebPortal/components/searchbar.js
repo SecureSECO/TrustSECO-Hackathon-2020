1de1800de6d8c47fe2c3d80526e6080ea17eb910
Jozef Siu <j.y.l.siu@students.uu.nl>
2020-11-19 16:13:20 +0100

    changed searchbar class to function and using nextjs router to execute search

--- NextJSWebPortal/components/searchbar.js
+++ NextJSWebPortal/components/searchbar.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { useRouter } from "next/router";
 import { InputGroup, Button } from "react-bootstrap";
 import Autosuggest from "react-autosuggest";
 // import getPackagePaths from "../lib/packageData"
@@ -49,82 +50,70 @@ function renderSuggestion(suggestion, { query }) {
   );
 }
 
-export default class Searchbar extends React.Component {
-  constructor(props) {
-    super();
-    packageNames = props.packageNames;
-    // Autosuggest is a controlled component.
-    // This means that you need to provide an input value
-    // and an onChange handler that updates this value (see below).
-    // Suggestions also need to be provided to the Autosuggest,
-    // and they are initially empty because the Autosuggest is closed.
-    this.state = {
-      value: "",
-      suggestions: [],
-    };
-  }
-  handleSubmit = () => {
-    if (this.state.value) {
-      window.location.href = `${
-        window.location.hostname
-      }/TrustSECO/package/${this.state.value.toLowerCase()}`;
+// export default class Searchbar extends React.Component {
+export default function Searchbar(props) {
+  // let history = useHistory();
+  const router = useRouter();
+
+  packageNames = props.packageNames;
+
+  const [value, setValue] = useState("");
+  const [name, setName] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+
+  const handleSubmit = () => {
+    if (value) {
+      // window.location.href = `${
+      //   window.location.hostname
+      // }/TrustSECO/package/${this.state.value.toLowerCase()}`;
+      router.push(`package/${value.toLowerCase()}`);
     }
   };
 
-  onChange = (event, { newValue }) => {
-    this.setState({
-      value: newValue,
-    });
+  const onChange = (event, { newValue }) => {
+    setValue(newValue);
   };
 
   // Autosuggest will call this function every time you need to update suggestions.
   // You already implemented this logic above, so just use it.
-  onSuggestionsFetchRequested = ({ value }) => {
-    this.setState({
-      suggestions: getSuggestions(value),
-    });
+  const onSuggestionsFetchRequested = ({ value }) => {
+    setSuggestions(getSuggestions(value));
   };
 
   // Autosuggest will call this function every time you need to clear suggestions.
-  onSuggestionsClearRequested = () => {
-    this.setState({
-      suggestions: [],
-    });
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([]);
   };
 
-  render() {
-    const { value, suggestions } = this.state;
-
-    // Autosuggest will pass through all these props to the input.
-    const inputProps = {
-      placeholder: "Package",
-      value,
-      onChange: this.onChange,
-    };
-    // let paths = getPackagePaths()
-    // console.log("paths", paths)
-    // let pathsexp = paths.map((record) => {
-    //     console.log("records")
-    //     console.log(record.title)
-    //     return(record.title)})
-    // console.log(pathsexp)
-    // Finally, render it!
-    return (
-      <InputGroup className="searchWrap">
-        <Autosuggest
-          suggestions={suggestions}
-          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-          getSuggestionValue={getSuggestionValue}
-          renderSuggestion={renderSuggestion}
-          inputProps={inputProps}
-        />
-        <InputGroup.Append>
-          <Button variant="outline-secondary" onClick={this.handleSubmit}>
-            Search
-          </Button>
-        </InputGroup.Append>
-      </InputGroup>
-    );
-  }
+  // Autosuggest will pass through all these props to the input.
+  const inputProps = {
+    placeholder: "Package",
+    value,
+    onChange: onChange,
+  };
+  // let paths = getPackagePaths()
+  // console.log("paths", paths)
+  // let pathsexp = paths.map((record) => {
+  //     console.log("records")
+  //     console.log(record.title)
+  //     return(record.title)})
+  // console.log(pathsexp)
+  // Finally, render it!
+  return (
+    <InputGroup className="searchWrap">
+      <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
+      />
+      <InputGroup.Append>
+        <Button variant="outline-secondary" onClick={handleSubmit}>
+          Search
+        </Button>
+      </InputGroup.Append>
+    </InputGroup>
+  );
 }
